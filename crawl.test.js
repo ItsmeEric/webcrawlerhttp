41a90164edeed21388e73bf5713219b4bf5dd2c6
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-03-14 01:11:21 +0200

    feature: return stripped hostname and pathname from url address

--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,9 @@
 const { normalizeURL } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
-test("normalizeURL", () => {
-  const input = "";
+test("normalizeURL strip protocol", () => {
+  const input = "https://blog.boot.dev/path";
   const actualOutput = normalizeURL(input);
-  const expectedOutput = "WrongUrl";
+  const expectedOutput = "blog.boot.dev/path";
   expect(actualOutput).toEqual(expectedOutput);
 });
