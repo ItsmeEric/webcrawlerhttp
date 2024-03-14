eccf3b9a96be36662b3964e585d9defdc0c64ea2
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-03-14 16:45:15 +0200

    feat: add logic to normalizeURL and tests

--- crawl.js
+++ crawl.js
@@ -1,7 +1,11 @@
 function normalizeURL(url) {
   const urlObj = new URL(url);
 
-  return `${urlObj.hostname}${urlObj.pathname}`;
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+
+  return hostPath.length > 0 && hostPath.slice(-1) === "/"
+    ? hostPath.slice(0, -1)
+    : hostPath;
 }
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -7,3 +7,24 @@ test("normalizeURL strip protocol", () => {
   const expectedOutput = "blog.boot.dev/path";
   expect(actualOutput).toEqual(expectedOutput);
 });
+
+test("normalizeURL strip trailing slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actualOutput = normalizeURL(input);
+  const expectedOutput = "blog.boot.dev/path";
+  expect(actualOutput).toEqual(expectedOutput);
+});
+
+test("normalizeURL capitals", () => {
+  const input = "https://BLOG.boot.dev/path";
+  const actualOutput = normalizeURL(input);
+  const expectedOutput = "blog.boot.dev/path";
+  expect(actualOutput).toEqual(expectedOutput);
+});
+
+test("normalizeURL strip http", () => {
+  const input = "http://blog.boot.dev/path/";
+  const actualOutput = normalizeURL(input);
+  const expectedOutput = "blog.boot.dev/path";
+  expect(actualOutput).toEqual(expectedOutput);
+});
